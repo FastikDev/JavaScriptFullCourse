0fd6a97312d4f2a57d45f57130f6482106c11585
FastikDev <sir.fastik18@gmail.com>
2024-02-13 00:18:28 +0200

    Working with numbers

---
+++ lesson-2/task-4/index.js
@@ -0,0 +1,9 @@
+// put your code here
+const a = 4;
+const b = 2;
+console.log('a:', a);
+console.log('b:', b);
+console.log('a - b =', a - b);
+console.log('a + b =', a + b);
+console.log('a / b =', a / b);
+console.log('a * b =', a * b);
