a72f4431b455d1cbcce78bc2833b025b0419e745
FastikDev <sir.fastik18@gmail.com>
2024-02-13 00:15:14 +0200

    Data Types

---
+++ lesson-2/task-2/index.js
@@ -0,0 +1,29 @@
+const a = 17;
+const b = 17.17;
+const c = '17';
+const d = '17';
+const e = "I'm a string";
+const f = false;
+const g = true;
+const h = null;
+const i = undefined;
+const j = Symbol("I'm a Symbol");
+const k = { name: 'John' };
+const l = [1, 2, 3, 4];
+const m = function run() {
+  return 17;
+};
+
+console.log('a:', typeof a);
+console.log('b:', typeof b);
+console.log('c:', typeof c);
+console.log('d:', typeof d);
+console.log('e:', typeof e);
+console.log('f:', typeof f);
+console.log('g:', typeof g);
+console.log('h:', typeof h);
+console.log('i:', typeof i);
+console.log('j:', typeof j);
+console.log('k:', typeof k);
+console.log('l:', typeof l);
+console.log('m:', typeof m);
