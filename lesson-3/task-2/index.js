997348b7b662c4c729e8a51d6fef3b52ed6a4082
FastikDev <sir.fastik18@gmail.com>
2024-02-13 01:02:08 +0200

    Type casting

---
+++ lesson-3/task-2/index.js
@@ -0,0 +1,30 @@
+// code below must be finished ;)
+
+/* cast to string */
+console.log('17 to string is ' + String(17));
+console.log('-17.17 to string is ' + String(-17.17));
+console.log('false to string is ' + String(false));
+console.log('null to string is ' + String(null));
+console.log('undefined to string is ' + String(undefined));
+console.log('0 to string is ' + String(0));
+
+/* cast to number */
+console.log("'17' to number is " + Number('17'));
+console.log('true to number is ' + Number(true));
+console.log('false to number is ' + Number(false));
+console.log('null to number is ' + Number(null));
+console.log('undefined to number is ' + Number(undefined));
+console.log("'   20   ' to number is " + Number('   20   '));
+console.log("'      ' to number is " + Number('      '));
+console.log("'   30d   ' to number is " + Number('   30d   '));
+
+/* cast to boolean */
+console.log('null to boolean is ' + Boolean(null));
+console.log('undefined to boolean is ' + Boolean(undefined));
+console.log('0 to boolean is ' + Boolean(0));
+console.log('-0 to boolean is ' + Boolean(-0));
+console.log('NaN to boolean is ' + Boolean(NaN));
+console.log("'' to boolean is " + Boolean(''));
+console.log("' ' to boolean is " + Boolean(' '));
+console.log('17 to boolean is ' + Boolean(17));
+console.log("'Hello' to boolean is " + Boolean('Hello'));
