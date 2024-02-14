867bede66bbecc7dd08b151da77d35a1c06ef8f7
FastikDev <sir.fastik18@gmail.com>
2024-02-14 02:13:00 +0200

    Таблица умножения

---
+++ lesson-4/task-3/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+for(let i = 1; i <= 5; i++) {
+    console.log('\n=> ' + i + ' <=\n\n');
+    for(let x = 1; x <= 10; x++) {
+        console.log(i + ' x ' + x + ' = ' + i * x);
+    }
+}
