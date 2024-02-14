7e5a5fbc52749a1097d575da8dbb305c908a0778
FastikDev <sir.fastik18@gmail.com>
2024-02-14 13:44:04 +0200

    First Function

---
+++ lesson-5/task-1/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} firstNum
+ * @param {number} secondNum
+ * @return {number}
+ */
+function sum(firstNum, secondNum) {
+  // put your code here
+  return firstNum + secondNum;
+}
+
+// examples
+console.log(sum(11, 11)); // ==> 22
+console.log(sum(-2, 10)); // ==> 8
