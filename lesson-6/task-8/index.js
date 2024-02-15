2d06e9cdffb165347bbe534f2bd7baa626d13c4a
FastikDev <sir.fastik18@gmail.com>
2024-02-15 14:27:35 +0200

    Increase evens

---
+++ lesson-6/task-8/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @param {number[]} arr
+ * @param {number} delta
+ * @return {number[]}
+ */
+const increaseEvenEl = (arr, delta) => {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return null;
+  }
+
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      newArr.push(arr[i] + delta);
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
+
+// examples
+console.log(increaseEvenEl([2, 5, 6, 8, 11, 9, 4], 20)); // ===> [22, 5, 26, 28, 11, 9, 24]
+console.log(increaseEvenEl([7, 11, 1], 10)); // ===> [7, 11, 1]
+console.log(increaseEvenEl([], 120)); // ===> []
