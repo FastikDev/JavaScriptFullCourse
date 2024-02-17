5e3e432c8f44f570944f3c0883c1d05302802414
FastikDev <sir.fastik18@gmail.com>
2024-02-17 22:02:35 +0200

    Object Filter

---
+++ lesson-8/task-7/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-restricted-syntax */
+
+/* Для решения этой задачи используй for..in цикл. В реальных проектах это плохая практика,
+ * лучше использовать методы класса Object - keys(), values(), entries(). Но мы с ними пока не познакомитись.
+ * Чтобы eslint не ругался на эту ошибку, для этой задачи он отключен аннотацией eslint-disable
+ * */
+
+const getAdults = obj => {
+  // put your code here
+  if (!Object.keys(obj).length) {
+    return {};
+  }
+
+  const oldPeople = {};
+
+  for (const key in obj) {
+    if (obj[key] >= 18) oldPeople[key] = obj[key];
+  }
+  return oldPeople;
+};
+
+// examples
+console.log(getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 })); // ==> { 'John Doe': 19, Bob: 18 }
+console.log(getAdults({ Ann: 56, Andrey: 7 })); // ==> { Ann: 56 }
+console.log(getAdults({}));
