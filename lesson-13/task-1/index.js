0a3244024e6319cc5fdec4cb0a3e4de1cdc028ae
FastikDev <sir.fastik18@gmail.com>
2024-03-28 10:35:53 +0200

    First tests with Jest

--- lesson-13/task-1/index.js
+++
@@ -1,6 +0,0 @@
-const cleanTransactionsList = transaction => {
-  return transaction.filter(item => Number(item)).map(item => '$' + Number(item).toFixed(2));
-};
-// Пример использования
-const transactions = [' 1.9', '16.4', 17, ' 1 dollar ', 'invalid', '12.345'];
-console.log(cleanTransactionsList(transactions));
--- lesson-13/task-1/index.test.js
+++ lesson-13/task-1/index.test.js
@@ -1,3 +1,5 @@
-it('17 везде 17', () => {
-  expect(17).toEqual(18);
+const getEvenNumber = arr => arr.filter(el => el % 2 > 0);
+
+it('17 равно 17', () => {
+  expect(17).toEqual(17);
 });
