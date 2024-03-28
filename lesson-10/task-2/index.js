edcad0daa40af519bc909d057fc3e63a0acf650f
FastikDev <sir.fastik18@gmail.com>
2024-03-28 10:06:17 +0200

    Парсинг чисел

---
+++ lesson-10/task-2/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// put your code here
+const getParsedIntegers = arr => arr.map(item => Number.parseInt(item));
+const getParsedIntegersV2 = arr => arr.map(item => parseInt(item));
+
+const getParsedFloats = arr => arr.map(item => Number.parseFloat(item));
+const getParsedFloatsV2 = arr => arr.map(item => parseFloat(item));
