a87d2619618f97fedf892a6ad3e0c2e584743933
FastikDev <sir.fastik18@gmail.com>
2024-04-02 00:13:53 +0300

    Test code in different files

---
+++ lesson-13/task-2/calculator.js
@@ -0,0 +1,4 @@
+export const getSquareNumbers = arr => arr.map(num => num * num);
+
+export const getOddNumbers = arr => arr.filter(num => num % 2 === 1);
+export default (a, b) => a + b;
---
+++ lesson-13/task-2/calculator.test.js
@@ -0,0 +1,17 @@
+import { rules } from 'eslint-config-airbnb-base';
+import getSum, { getSquareNumbers, getOddNumbers } from './calculator';
+
+it('should get square numbers', () => {
+  const result = getSquareNumbers([1, 2, 3]);
+  expect(result).toEqual([1, 4, 9]);
+});
+
+it('shoud keep odd number only', () => {
+  const result = getOddNumbers([1, 2, 3, 4, 5]);
+  expect(result).toEqual([1, 3, 5]);
+});
+
+it('get sum of numbers', () => {
+  const result = getSum(8, 4);
+  expect(result).toEqual(12);
+});
