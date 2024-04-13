e11bcdd30ba9db13817b791598717105c7880eaa
FastikDev <sir.fastik18@gmail.com>
2024-04-13 21:44:14 +0300

    Корабль - прототипы

---
+++ lesson-19/task-1/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// put your code here
+export const vehicle = {
+  name: 'Argo',
+  move() {
+    console.log(`${this.name} is moving`);
+  },
+  stop() {
+    console.log(`${this.name} stopped`);
+  },
+};
+
+export const ship = {
+  __proto__: vehicle,
+  startMachine() {
+    console.log(`${this.name} lifting anchor up`);
+    this.move();
+  },
+  stopMachine() {
+    console.log(`${this.name} lifting anchor down`);
+    this.stop();
+  },
+};
