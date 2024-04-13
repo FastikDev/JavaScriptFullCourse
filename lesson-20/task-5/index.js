9415788a05910ba20a3d8184593fe1d8b04c7661
FastikDev <sir.fastik18@gmail.com>
2024-04-13 21:57:16 +0300

    User Repository

---
+++ lesson-20/task-5/index.js
@@ -0,0 +1,56 @@
+/* eslint-disable max-classes-per-file */
+
+export class User {
+  constructor(id, name, sessionId) {
+    this._id = id;
+    this._name = name;
+    this._sessionId = sessionId;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get sessionId() {
+    return this._sessionId;
+  }
+}
+
+export class UserRepository {
+  constructor(users) {
+    this._users = Object.freeze(users);
+  }
+
+  get users() {
+    return this._users;
+  }
+
+  getUserNames() {
+    return this._users.map(user => user.name);
+  }
+
+  getUserIds() {
+    return this._users.map(user => user.id);
+  }
+
+  getUserNameById(id) {
+    const user = this._users.find(user => user.id === id);
+    return user ? user.name : 'User not found';
+  }
+}
+
+// examples
+const user = new User('1', 'Tom', 'session-id');
+
+// получить свойства можем
+console.log(user.id); // ===> '1'
+console.log(user.name); // ===> 'Tom'
+console.log(user.sessionId); // ===> 'session-id'
+
+// но изменить эти свойства нельзя
+// user.name = 'Bob'; // пытаемся изменить старое значение
+// console.log(user.name); // ===> 'Tom' - но изменение проигнорировано, так как setter отсутствует
