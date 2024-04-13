27731aa4a0deecfa2787fbe4661a31131d106a09
FastikDev <sir.fastik18@gmail.com>
2024-04-13 21:25:48 +0300

    Контекст выполнения функции

---
+++ lesson-17/task-1/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export const user = {
+  // put your code here
+  firstName: 'John',
+  lastName: 'Doe',
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
