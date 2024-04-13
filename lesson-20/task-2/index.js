626b777b242153c76653f528ac6f62bc139e9b34
FastikDev <sir.fastik18@gmail.com>
2024-04-13 21:52:49 +0300

    Корабль

---
+++ lesson-20/task-2/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// put your code here
+export class Vehicle {
+  constructor(name, numberOfWheels) {
+    this.name = name;
+    this.numberOfWheels = numberOfWheels;
+  }
+
+  move() {
+    console.log(`${this.name} is moving`);
+  }
+
+  stop() {
+    console.log(`${this.name} stopped`);
+  }
+}
+
+export class Ship extends Vehicle {
+  constructor(name, numberOfWheels, maxSpeed) {
+    super(name, numberOfWheels);
+    this.maxSpeed = maxSpeed;
+  }
+
+  move() {
+    console.log(`${this.name} lifting anchor up`);
+    super.move();
+  }
+
+  stop() {
+    super.stop();
+    console.log(`${this.name} lifting anchor down`);
+  }
+}
+const ship = new Ship('Argo', 3, 40);
