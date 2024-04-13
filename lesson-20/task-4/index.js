19f0eae57cd219dcc9e3dedf127dff861861766a
FastikDev <sir.fastik18@gmail.com>
2024-04-13 21:55:06 +0300

    Order

---
+++ lesson-20/task-4/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * @field {string} id
+ * @field {number} price
+ * @field {date} dateCreated
+ * @field {boolean} isConfirmed
+ * @field {date} dateConfirmed
+ * @field {string} city
+ * @field {string} type
+ */
+export class Order {
+  constructor(price, city, type) {
+    this.id = this.generateOrderId();
+    this.price = price;
+    this.dateCreated = new Date();
+    this.isConfirmed = false;
+    this.dateConfirmed = null;
+    this.city = city;
+    this.type = type;
+  }
+
+  generateOrderId() {
+    return Date.now().toString();
+  }
+
+  checkPrice() {
+    return this.price > 1000;
+  }
+
+  confirmOrder() {
+    if (!this.isConfirmed) {
+      this.isConfirmed = true;
+      this.dateConfirmed = new Date();
+    }
+  }
+
+  isValidType() {
+    return this.type === 'Buy' || this.type === 'Sell';
+  }
+}
+
+// Пример использования класса:
+const order1 = new Order(1200, 'New York', 'Buy');
+console.log(order1);
