bb49b29620c7102bfee448c826420096ed9a6d79
FastikDev <149237046+FastikDev@users.noreply.github.com>
2024-04-15 00:57:51 +0300

    Поиск элемента на странице

---
+++ lesson-21/task-2/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+export const getTitleElement = () => {
+  const titleElem = document.querySelector('.title'):
+  console.dir(titleElem);
+  return titleElem;
+};
+
+export const getInputElement = () => {
+  const inputElem = document.querySelector('input[type="text"]');
+  console.dir(inputElem);
+  return inputElem;
+};
