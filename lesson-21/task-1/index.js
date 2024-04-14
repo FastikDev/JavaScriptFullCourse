dd0905e4a599cf9395af64ea248aede52a52f00c
FastikDev <149237046+FastikDev@users.noreply.github.com>
2024-04-14 23:50:27 +0300

    Todo List - render list

---
+++ lesson-21/task-1/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
+
+const renderTasks = tasksList => {
+  const listElem = document.querySelector('.list');
+  const listItemsElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      const checkboxElem = document.createElement('input');
+      checkboxElem.setAttribute('type', 'checkbox');
+      checkboxElem.checked = done;
+      checkboxElem.classList.add('list__item-checkbox');
+      listItemElem.append(checkboxElem, text);
+      
+      return listItemElem;
+    });
+
+  listElem.append(...listItemsElems);
+};
+
+renderTasks(tasks);
