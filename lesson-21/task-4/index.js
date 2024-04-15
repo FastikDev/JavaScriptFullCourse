f0d18cb1d56071bb8e7523293af96ee90ceb7b12
FastikDev <149237046+FastikDev@users.noreply.github.com>
2024-04-15 11:15:40 +0300

    Получение содержимого элемента

---
+++ lesson-21/task-4/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+export const getTitle = () => {
+  const titleElem = document.querySelector('.title');
+
+  return titleElem.textContent;
+};
+
+export const getDescription = () => {
+  const aboutElem = document.querySelector('.about');
+
+  return aboutElem.innerText;
+};
+
+export const gePlans = () => {
+  const plansElem = document.querySelector('.plans');
+
+  return plansElem.innerHTML;
+};
+
+export const getGoal = () => {
+  const goalElem = document.querySelector('.goal');
+  
+  return goalElem.outerHTML;
+};
