aea5e5e869fc960fec45f627081ce615e09eaa42
FastikDev <sir.fastik18@gmail.com>
2024-04-16 01:31:22 +0300

    Дата атрибуты (data-attributes)

---
+++ lesson-21/task-12/index.js
@@ -0,0 +1,9 @@
+const squaredNumbers = () => {
+  const numberElems = document.querySelectorAll('.number');
+
+  numberElems.forEach(num => {
+    const originalNumber = num.dataset.number; // Исправлено: originalNumber
+    const squaredNumber = originalNumber * originalNumber;
+    num.dataset.squaredNumber = squaredNumber;
+  });
+};
