a9c7194b97388359c0000829f9243e9c6cedfc5e
FastikDev <sir.fastik18@gmail.com>
2024-04-16 01:04:31 +0300

    update

--- lesson-21/task-10/index.js
+++ lesson-21/task-10/index.js
@@ -2,6 +2,7 @@ export const finishForm = () => {
   const formElem = document.querySelector('.login-form');
   const inputElem = document.createElement('input');
   inputElem.setAttribute('name', 'login');
+  inputElem.setAttribute('type', 'text');
   formElem.prepend(inputElem);
 
   const passElem = document.querySelector('input[name="password"]');
