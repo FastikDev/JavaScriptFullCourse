43225839949a5266f75b127267942bbbde697827
FastikDev <149237046+FastikDev@users.noreply.github.com>
2024-04-18 02:37:44 +0300

    Update index.js

--- lesson-22/task-7/index.js
+++ lesson-22/task-7/index.js
@@ -5,5 +5,5 @@ const inputElem = document.querySelector('.text-input');
 inputElem.addEventListener('change', handler);
 
 function handler(event) {
-  console.log(event.target.value;
+  console.log(event.target.value);
 }
