3fee4d56fb67502ccabaa078478a95ca542565b4
FastikDev <149237046+FastikDev@users.noreply.github.com>
2024-04-18 02:33:16 +0300

    Create index.js

---
+++ lesson-22/task-7/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const inputElem = document.querySelector('.text-input');
+
+inputElem.addEventListener('change', handler);
+
+function handler(event) {
+  console.log(event.target.value;
+}
