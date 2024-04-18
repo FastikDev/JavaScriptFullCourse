dc4c37e29a9ceb6d0270b5d33d8bc097e2c91a04
FastikDev <sir.fastik18@gmail.com>
2024-04-19 01:59:06 +0300

    Делегирование

---
+++ lesson-23/task-2/index.js
@@ -0,0 +1,57 @@
+const generateNumbersRange = (from, to) => {
+  const result = [];
+
+  for (let i = from; i <= to; i++) {
+    result.push(i);
+  }
+
+  return result;
+};
+
+const getLineSeats = () => {
+  generateNumbersRange(1, 10)
+    .map(
+      seatNumber => `
+    <div 
+        class="sector__line" 
+        data-seat-number="${seatNumber}"
+        ></div>
+    `,
+    )
+    .join('');
+};
+
+const getSectorLines = () => {
+  const seatString = getLineSeats();
+
+  generateNumbersRange(1, 10)
+    .map(
+      lineNumber => `
+    <div 
+        class="sector__line" 
+        data-line-number="${lineNumber}"
+        >${seatString}</div>
+    `,
+    )
+    .join('');
+};
+
+const renderArena = () => {
+  const arenaElem = document.querySelector('.arena');
+  const lineString = getSectorLines();
+
+  const sectorsString = generateNumbersRange(1, 3)
+    .map(
+      sectorNumber => `
+    <div 
+        class="sector" 
+        data-sector-number="${sectorNumber}"
+        >${lineString}</div>
+    `,
+    )
+    .join('');
+
+  arenaElem.innerHTML = sectorsString;
+};
+
+renderArena();
