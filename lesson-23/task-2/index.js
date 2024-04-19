c8679da8d596aa403fd97d4cc697d8633255d4a2
FastikDev <sir.fastik18@gmail.com>
2024-04-19 18:06:59 +0300

    Делегирование

--- lesson-23/task-2/index.js
+++ lesson-23/task-2/index.js
@@ -66,7 +66,7 @@ const onSeatSelect = event => {
 
   const selectedSeatElem = document.querySelector('.board__selected-seat');
 
-  selectedSeatElem.innerText = `S ${sectorNumbers}  L - ${lineNumbers} -  S ${seatNumbers}`;
+  selectedSeatElem.innerText = `S ${seatNumbers}  L - ${lineNumbers} -  S ${sectorNumbers}`;
 };
 
 arenaElem.addEventListener('click', onSeatSelect);
