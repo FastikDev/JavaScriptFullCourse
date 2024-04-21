2aceedfa640757275a744943b2af5495127d2e8f
FastikDev <149237046+FastikDev@users.noreply.github.com>
2024-04-21 10:07:38 +0300

    Update index.js

--- lesson-24/task-1/index.js
+++ lesson-24/task-1/index.js
@@ -1,6 +1,6 @@
 const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
 
-const dayOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
   const day = new Date(date).getDate();
 
   const dateInFuture = new Date(date).setDate(day + days);
