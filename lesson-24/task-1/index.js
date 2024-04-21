5c76aace047f08a99df090d236461b9bebd1729b
FastikDev <149237046+FastikDev@users.noreply.github.com>
2024-04-21 10:05:55 +0300

    Create index.js

---
+++ lesson-24/task-1/index.js
@@ -0,0 +1,9 @@
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+
+const dayOfWeek = (date, days) => {
+  const day = new Date(date).getDate();
+
+  const dateInFuture = new Date(date).setDate(day + days);
+
+  return weekDays[new Date(dataInFuture).getDay()];
+};
