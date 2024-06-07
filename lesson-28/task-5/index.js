07f447ed03744d77b362d109498a893f145041f3
AlinkaSuper <sir.fastik18@gmail.com>
2024-06-08 00:22:10 +0300

    Date Recoursing

---
+++ lesson-28/task-5/index.js
@@ -0,0 +1,67 @@
+const shmoment = date => {
+    let newDate = new Date(date);
+
+    return {
+        add(key, value) {
+            switch (key) {
+                case 'years':
+                    newDate.setFullYear(newDate.getFullYear() + value);
+                    break;
+                case 'months':
+                    newDate.setMonth(newDate.getMonth() + value);
+                    break;
+                case 'days':
+                    newDate.setDate(newDate.getDate() + value);
+                    break;
+                case 'hours':
+                    newDate.setHours(newDate.getHours() + value);
+                    break;
+                case 'minutes':
+                    newDate.setMinutes(newDate.getMinutes() + value);
+                    break;
+                case 'seconds':
+                    newDate.setSeconds(newDate.getSeconds() + value);
+                    break;
+                case 'milliseconds':
+                    newDate.setMilliseconds(newDate.getMilliseconds() + value);
+                    break;
+            }
+            return this;
+        },
+        subtract(key, value) {
+            switch (key) {
+                case 'years':
+                    newDate.setFullYear(newDate.getFullYear() - value);
+                    break;
+                case 'months':
+                    newDate.setMonth(newDate.getMonth() - value);
+                    break;
+                case 'days':
+                    newDate.setDate(newDate.getDate() - value);
+                    break;
+                case 'hours':
+                    newDate.setHours(newDate.getHours() - value);
+                    break;
+                case 'minutes':
+                    newDate.setMinutes(newDate.getMinutes() - value);
+                    break;
+                case 'seconds':
+                    newDate.setSeconds(newDate.getSeconds() - value);
+                    break;
+                case 'milliseconds':
+                    newDate.setMilliseconds(newDate.getMilliseconds() - value);
+                    break;
+            }
+            return this;
+        },
+        result() {
+            return newDate;
+        },
+    };
+};
+
+const result = shmoment(new Date(2020, 0, 7, 17, 17, 17))
+    .add('days', 8)
+    .subtract('years', 1)
+    .result();
+console.log(result);
