789c5b544e5397168d4bf7fc6d1bbd011375e626
AlinkaSuper <sir.fastik18@gmail.com>
2024-06-10 12:19:30 +0300

    Fix Promise Handler

---
+++ lesson-31/task-3/index.js
@@ -0,0 +1,38 @@
+const successPromise = new Promise(resolve => {
+  resolve(32);
+});
+
+/*
+ * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+
+successPromise
+  .then(number => {
+    const halfNumber = number / 2;
+    return halfNumber;
+  })
+  .then(number => {
+    /* fix this handler */
+    const squaredNumber = number * number;
+    return squaredNumber;
+  })
+  .then(result => {
+    console.log(result); // 256
+  });
+
+/*
+ * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+successPromise
+  .then(number => {
+    /* fix this handler */
+    const result = number * 10;
+    return number;
+  })
+  .then(result => {
+    console.log(result); // 320
+  });
+
+console.log(
+  '!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!',
+);
