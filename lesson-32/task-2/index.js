b745837903ddb4c0740bce910f9268be62c8dc9e
AlinkaSuper <sir.fastik18@gmail.com>
2024-06-11 01:07:44 +0300

    Promise race

---
+++ lesson-32/task-2/index.js
@@ -0,0 +1,39 @@
+const getRandomNumber = (from, to) =>
+    from + Math.random() * (to - from);
+
+const request = url => new Promise((resolve) => {
+    const randomDelay = getRandomNumber(1000, 3000);
+    
+    setTimeout(() => {
+        resolve({
+            userData: {
+                name: 'TOM',
+                age: 17,
+                source: url,
+            }
+        });
+    }, randomDelay);
+});
+
+const servers = [
+    'https://server.com/us',
+    'https://server.com/eu',
+    'https://server.com/au',
+    
+]
+
+export const getuserASAP = userId => {
+    const usersUrls = servers
+        .map(serverUrl => `${serverUrl}/users/${userId}`);
+
+    const requests = usersUrls 
+        .map(userUrl => request(userUrl))
+
+    return Promise.race(requests)
+};
+
+getuserASAP('user-id-1').then(res => console.log(res));
+
+
+
+ 
