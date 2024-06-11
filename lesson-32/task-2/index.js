7a2e4ee1063e80c5463df0d8e8cd7fead407491d
FastikDev <sir.fastik18@gmail.com>
2024-06-11 13:39:40 +0300

    Promise race

--- lesson-32/task-2/index.js
+++ lesson-32/task-2/index.js
@@ -1,36 +1,27 @@
-const getRandomNumber = (from, to) =>
-    from + Math.random() * (to - from);
+const getRandomNumber = (from, to) => from + Math.random() * (to - from);
 
-const request = url => new Promise((resolve) => {
+const request = url =>
+  new Promise(resolve => {
     const randomDelay = getRandomNumber(1000, 3000);
-    
     setTimeout(() => {
-        resolve({
-            userData: {
-                name: 'TOM',
-                age: 17,
-                source: url,
-            }
-        });
+      resolve({
+        userData: {
+          name: 'Tom',
+          age: 17,
+        },
+        source: url,
+      });
     }, randomDelay);
-});
+  });
 
-const servers = [
-    'https://server.com/us',
-    'https://server.com/eu',
-    'https://server.com/au',
-];
+const servers = ['https://server.com/us', 'https://server.com/eu', 'https://server.com/au'];
 
 export const getUserASAP = userId => {
-    const usersUrls = servers
-        .map(serverUrl => `${serverUrl}/users/${userId}`);
+  const userUrl = servers.map(serverUrl => `${serverUrl}/users/${userId}`);
 
-    const requests = usersUrls
-        .map(userUrl => request(userUrl));
+  const requests = userUrl.map(userUrl => request(userUrl));
 
-    return Promise.race(requests);
+  return Promise.race(requests);
 };
 
-
-
- 
+getUserASAP('user-1').then(res => console.log(res));
