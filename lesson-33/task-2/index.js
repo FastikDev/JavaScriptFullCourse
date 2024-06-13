f439121f79b58ac07fa340aeea610fac42b395b4
AlinkaSuper <sir.fastik18@gmail.com>
2024-06-13 19:09:49 +0300

    get task info

---
+++ lesson-33/task-2/index.js
@@ -0,0 +1,19 @@
+//algo
+
+//1. make request to serer
+//2. get json from responcive
+//3. make find id from responcive array
+
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
+
+function getTasksList() {
+  // put your code here
+  return fetch(baseUrl)
+    .then(responce => responce.json());
+}
+
+function getTaskById(taskId) {
+  // put your code here
+  return getTasksList()
+    .then(tasksList => tasksList.find(task => task.id === taskId.toString()));
+}
