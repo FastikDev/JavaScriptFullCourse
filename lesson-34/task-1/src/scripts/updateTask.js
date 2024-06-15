12f2428da8ec004ff012b28d7398732a94679771
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2024-06-16 01:50:02 +0300

    Update updateTask.js

--- lesson-34/task-1/src/scripts/updateTask.js
+++ lesson-34/task-1/src/scripts/updateTask.js
@@ -1,34 +1,46 @@
 import { renderTasks } from './renderer.js';
 import { getItem, setItem } from './storage.js';
-import { updateTask, getTasksList } from './tasksGateway.js';
+import { updateTask, getTasksList, deleteTask } from './tasksGateway.js';
 
 export const onToggleTask = e => {
   const isCheckbox = e.target.classList.contains('list__item-checkbox');
+  const isDeleteButton = e.target.classList.contains('list-item__delete-btn');
 
-  if (!isCheckbox) {
+  if (isCheckbox) {
+    const taskId = e.target.dataset.id;
+    const tasksList = getItem('tasksList');
+    const task = tasksList.find(task => task.id === taskId);
+    const { text, createDate } = task;
+    const done = e.target.checked;
+
+    const updatedTask = {
+      text,
+      createDate,
+      done,
+      finishDate: done ? new Date().toISOString() : null,
+    };
+
+    updateTask(taskId, updatedTask)
+      .then(() => getTasksList())
+      .then(newTasksList => {
+        setItem('tasksList', newTasksList);
+        renderTasks();
+      });
+
     return;
   }
 
-  const taskId = e.target.dataset.id;
-  const tasksList = getItem('tasksList');
-  const task = tasksList.find(task => task.id === taskId);
-  
-  const { text, createDate } = tasksList.find(task => task.id === taskId);
-  const done = e.target.checked;
+  if (isDeleteButton) {
+    const taskId = e.target.dataset.id;
 
-  const updatedTask = {
-    text,
-    createDate,
-    done,
-    finishDate: done ? new Date().toISOString() : null,
-  };
+    deleteTask(taskId)
+      .then(() => getTasksList())
+      .then(newTasksList => {
+        setItem('tasksList', newTasksList);
+        renderTasks();
+      });
 
-  updateTask(taskId, updatedTask)
-    .then(() => getTasksList())
-    .then(newTasksList => {
-      setItem('tasksList', newTasksList);
-      renderTasks();
-    });
+    return;
+  }
 };
-
 
