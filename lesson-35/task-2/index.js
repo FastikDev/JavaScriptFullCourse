301551b1bae5f1ac17683f7ca52ca198dfb859b6
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2024-06-17 00:16:45 +0300

    Update index.js

--- lesson-35/task-2/index.js
+++ lesson-35/task-2/index.js
@@ -14,7 +14,7 @@ const failRequest = Promise.reject(new Error('Something went wrong'));
 failRequest
   .catch(function onError2(error) {
     console.log('onError2', error.message);
-    throw new Error('Server error');
+     return Promise.reject(new Error('Server error'));
   })
   .then(function onSuccess2(data) {
     console.log('onSuccess2', data);
