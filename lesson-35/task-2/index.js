0f1d9111b85335c86df5528736f28d2f99f58bd9
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2024-06-17 00:35:42 +0300

    Update index.js

--- lesson-35/task-2/index.js
+++ lesson-35/task-2/index.js
@@ -3,7 +3,7 @@ const successRequest = Promise.resolve({ name: "Tom" });
 successRequest
   .then(function onSuccess1(data) {
     console.log(data);
-    throw new Error('Error with data');
+    throw new Error('Error with data'); // (1)
   })
   .catch(function onError1(error) {
     console.error('onError1', error.message);
@@ -12,13 +12,13 @@ successRequest
 const failRequest = Promise.reject(new Error('Something went wrong'));
 
 failRequest
-  .catch(function onError2 (error) {
+  .catch(function onError2(error) {
     console.log('onError2', error.message);
-  throw new Error('Server error');
+    throw new Error('Server error'); // (2)
   })
-  .then (function onSuccess2(data) {
- console.log('onSuccess2', data);
-})
-  .catch(function onError3 (error) {
-console.error ('onError3', error.message);
-});
+  .then(function onSuccess2(data) {
+    console.log('onSuccess2', data);
+  })
+  .catch(function onError3(error) {
+    console.error('onError3', error.message);
+  });
