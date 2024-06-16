1e7c06045a133d36bca3cabc5ef055b2075c1de4
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2024-06-16 16:08:14 +0300

    Update index.js

--- lesson-34/task-3/index.js
+++ lesson-34/task-3/index.js
@@ -11,49 +11,52 @@
 //4.5 clear form before submit
 //5. update button attibute disable before submit
 
-document.addEventListener('DOMContentLoaded', () => {
-  const formElem = document.querySelector('.login-form');
-  const submitButton = formElem.querySelector('.submit-button');
-  const inputs = formElem.querySelectorAll('input');
+const formElem = document.querySelector('.login-form');
+const submitButton = formElem.querySelector('.submit-button');
+const inputs = formElem.querySelectorAll('input');
 
-  const baseUrl = 'https://666e7390f1e1da2be5204c0e.mockapi.io/api/v1/register';
+const baseUrl = 'https://666e7390f1e1da2be5204c0e.mockapi.io/api/v1/register';
 
-  const checkFormValidity = () => {
-    const isValid = [...inputs].every(input => input.checkValidity());
-    submitButton.setAttribute('disabled', !isValid);
-  };
+const checkFormValidity = () => {
+  const isValid = [...inputs].every(input => input.checkValidity());
+  
+if (isValid) {
+    submitButton.removeAttribute('disabled');
+  } else {
+    submitButton.setAttribute('disabled', 'true');
+  }
+};
 
-  const handleInput = () => {
-    checkFormValidity();
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(formElem);
-    const formObject = Object.fromEntries(formData.entries());
-    const formJSON = JSON.stringify(formObject);
-
-    fetch(baseUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: formJSON
-    })
-    .then(response => response.json())
-    .then(data => {
-      alert(JSON.stringify(data));
-      formElem.reset();
-      checkFormValidity();
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      alert('Error creating user');
-    });
-  };
-
-  inputs.forEach(input => input.addEventListener('input', handleInput));
-  formElem.addEventListener('submit', handleSubmit);
+const handleInput = () => {
   checkFormValidity();
-});
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(formElem);
+  const formObject = Object.fromEntries(formData.entries());
+  const formJSON = JSON.stringify(formObject);
+
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: formJSON
+  })
+  .then(response => response.json())
+  .then(data => {
+    alert(JSON.stringify(data));
+    formElem.reset();
+    checkFormValidity();
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert('Error creating user');
+  });
+};
+
+inputs.forEach(input => input.addEventListener('input', handleInput));
+formElem.addEventListener('submit', handleSubmit);
+checkFormValidity();
