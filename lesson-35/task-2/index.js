5f3e9ac79a19692f9d5baae2c2c499c5de1770c2
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2024-06-16 23:46:38 +0300

    Promise Error

---
+++ lesson-35/task-2/index.js
@@ -0,0 +1,24 @@
+const successRequest = Promise.resolve({ name: "Tom" });
+
+successRequest
+  .then(function onSuccess1(data) {
+    console.log(data);
+    throw new Error('Error with data');
+  })
+  .catch(function onError1(error) {
+    console.error('onError1', error.message);
+  });
+
+const failRequest = Promise.reject(new Error('Something went wrong'));
+
+failRequest
+  .catch(function onError2 (error) {
+    console.log('onError2', error.message);
+  throw new Error('Server error');
+  })
+  .then (function onSuccess2(data) {
+ console.log('onSuccess2', data);
+})
+  .catch(function onError3 (error) {
+console.error ('onError3', error.message);
+});
