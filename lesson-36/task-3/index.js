364e1916523131d85ec5b13fcd5d3e5efe802164
FastikDev <sir.fastik18@gmail.com>
2024-06-18 01:08:42 +0300

    users blog

--- lesson-36/task-3/index.js
+++ lesson-36/task-3/index.js
@@ -1,5 +1,15 @@
 'use strict';
 
+//algo
+
+//1. Edit block try - catch
+//2. iteration on users array and response for all user
+//3. return user obj on every iteration
+//4. if response not ok reurn from this itetation
+//5. await Promise all for iteration array
+//6. iteration promise array for search [key] - blog
+//7. filter all user who do not have  [key] - blog
+
 const baseUrl = 'https://api.github.com/users';
 
 export const getUsersBlogs = async users => {
