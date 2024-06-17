065e96380480bb5a1a19fbe9c7bbfa0f32f0b451
FastikDev <sir.fastik18@gmail.com>
2024-06-17 20:12:30 +0300

    Error fix

--- lesson-35/task-2/index.js
+++ lesson-35/task-2/index.js
@@ -2,8 +2,7 @@ const successRequest = Promise.resolve({ name: 'Tom' });
 
 successRequest
   .then(function onSuccess1(data) {
-    console.log(data);
-    throw new Error('Error with data'); // (1)
+    throw new Error('Error with data');
   })
   .catch(function onError1(error) {
     console.error('onError1', error.message);
@@ -13,11 +12,11 @@ const failRequest = Promise.reject(new Error('Something went wrong'));
 
 failRequest
   .catch(function onError2(error) {
-    console.log('onError2', error.message);
-    throw new Error('Server error'); // (2)
+    console.error('onError2', error.message);
+    throw new Error('Server error');
   })
-  .then(function onSuccess2() {
-    console.log('not work');
+  .then(function onSuccess2(data) {
+    console.log('onSuccess2', data);
   })
   .catch(function onError3(error) {
     console.error('onError3', error.message);
