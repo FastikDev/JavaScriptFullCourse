c457d16d4e6c348b587dbc0e037e8178b3bde6a2
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2024-08-16 14:40:50 +0300

    Update index.js

--- lesson-34/task-3/index.js
+++ lesson-34/task-3/index.js
@@ -20,8 +20,7 @@ inputs.forEach(input => input.addEventListener('input', checkFormValidity));
 formElem.addEventListener('submit', async event => {
   event.preventDefault();
 
-  const formData = new FormData(formElem);
-  const data = Object.fromEntries(formData.entries());
+  const data = Object.fromEntries(new FormData(formElem).entries());
 
   try {
     const response = await fetch(baseUrl, {
