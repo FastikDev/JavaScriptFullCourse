c198d8cf6cf40882c39df79f46108513a125fc35
Viktor Troyan <149237046+FastikDev@users.noreply.github.com>
2024-08-19 22:25:16 +0300

    Update index.js

--- lesson-34/task-3/index.js
+++ lesson-34/task-3/index.js
@@ -1,21 +1,12 @@
 const formElem = document.querySelector('.login-form');
 const submitButton = formElem.querySelector('.submit-button');
-const inputs = formElem.querySelectorAll('input');
-
 const baseUrl = 'https://666e7390f1e1da2be5204c0e.mockapi.io/api/v1/register';
 
 const checkFormValidity = () => {
-  const isValid = [...inputs].every(input => input.checkValidity());
-  const buttonIsDisabled = submitButton.hasAttribute('disabled');
-
-  if (isValid && buttonIsDisabled) {
-    submitButton.removeAttribute('disabled');
-  } else if (!isValid && !buttonIsDisabled) {
-    submitButton.setAttribute('disabled', 'true');
-  }
+  submitButton.disabled = !formElem.reportValidity();
 };
 
-inputs.forEach(input => input.addEventListener('input', checkFormValidity));
+formElem.addEventListener('input', checkFormValidity);
 
 formElem.addEventListener('submit', async event => {
   event.preventDefault();
